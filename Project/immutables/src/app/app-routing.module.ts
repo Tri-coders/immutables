c39e41da60030233ff797482ea11aa518979f55b
AFIF <46858532+Afif79@users.noreply.github.com>
2020-05-08 16:26:33 +0530

    routing done

--- Project/immutables/src/app/app-routing.module.ts
+++ Project/immutables/src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginappComponent } from './loginapp/loginapp.component';
+import { RegistrationappComponent } from './registrationapp/registrationapp.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Home', component: HomeComponent},
+  {path: 'Login', component: LoginappComponent},
+  {path: 'Register', component: RegistrationappComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingcomponents = [HomeComponent, LoginappComponent, RegistrationappComponent]
--- Project/immutables/src/app/app.module.ts
+++ Project/immutables/src/app/app.module.ts
@@ -1,19 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingcomponents} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
-import { LoginappComponent } from './loginapp/loginapp.component';
-import { RegistrationappComponent } from './registrationapp/registrationapp.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginappComponent,
-    RegistrationappComponent
+    routingcomponents
   ],
   imports: [
     BrowserModule,
