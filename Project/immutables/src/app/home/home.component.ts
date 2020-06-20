da8859d39378e11e55b9d6f6b655afbcb968e4ec
AFIF <46858532+Afif79@users.noreply.github.com>
2020-06-20 22:27:25 +0530

    fab added

--- Project/immutables/src/app/home/home.component.ts
+++ Project/immutables/src/app/home/home.component.ts
@@ -12,4 +12,29 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  toggleFab(){
+    const Btns = document.querySelector(".btns");
+    const add = document.getElementById("add");
+    const remove = document.getElementById("remove");
+    const btn = document.querySelector(".btns").querySelectorAll("a");
+    Btns.classList.toggle("open");
+    if (Btns.classList.contains("open")) {
+      remove.style.display = "block";
+      add.style.display = "none";
+      btn.forEach((e, i) => {
+        setTimeout(() => {
+          var top = 50 * i;
+          e.style.top = top + "px";
+          console.log(e);
+        }, 100 * i);
+      });
+    } else {
+      add.style.display = "block";
+      remove.style.display = "none";
+      btn.forEach((e, i) => {
+        e.style.top = "0px";
+      });
+    }
+  }
+
 }
