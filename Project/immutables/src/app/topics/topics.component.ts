d233c7f9badaca81f2c5a9d02f491b9f91b79c97
AFIF <shaikhafif48@gmail.com>
2021-02-09 13:01:21 +0530

    topics page onload issue resovled

--- Project/immutables/src/app/topics/topics.component.ts
+++ Project/immutables/src/app/topics/topics.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, Event, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-topics',
@@ -7,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopicsComponent implements OnInit {
 
-  constructor() { }
+  constructor( router: Router) {    router.events
+    .pipe(filter((routerEvent: Event) => routerEvent instanceof NavigationEnd))
+    .subscribe(() => window.scrollTo(0, 0)); }
 
   ngOnInit(): void {
   }
