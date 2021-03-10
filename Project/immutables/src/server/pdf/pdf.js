a4587dd0f2d2342b8077d79ba06a02f6701a15b4
meetsanket24@gmail.com <meetsanket24@gmail.com>
2021-03-10 17:10:33 +0530

    files added and API done for pdf display

--- Project/immutables/src/server/pdf/pdf.js
+++ Project/immutables/src/server/pdf/pdf.js
@@ -5,6 +5,25 @@ var cookieParser = require('cookie-parser');
 pdf.use(cookieParser());
 pdf.use(cors())
 
+fileSystem = require('fs'),
+path = require('path');
+
+pdf.get('/pdfname', (req, res) => {
+    var sank = req.query.name;
+    console.log(sank)
+    var filePath = path.join(__dirname, '../../../../../Material/'+sank);
+    var stat = fileSystem.statSync(filePath);
+
+    res.writeHead(200, {
+        'Content-Type': 'application/pdf',
+        'Content-Length': stat.size
+    });
+
+    var readStream = fileSystem.createReadStream(filePath);
+    // We replaced all the event handlers with a simple call to readStream.pipe()
+    readStream.pipe(res);
+})
+
 pdf.post('/pdfname', (req,res)=>{
     //console.log(req.body.name)
     res.cookie("file_name_to_display", req.body.name); 
