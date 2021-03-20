127a0008764f8cd7cdbbf1833594a0b89f6babf2
meetsanket24@gmail.com <meetsanket24@gmail.com>
2021-03-20 16:40:40 +0530

    video api added

--- Project/immutables/src/server/pdf/pdf.js
+++ Project/immutables/src/server/pdf/pdf.js
@@ -32,4 +32,35 @@ pdf.post('/pdfname', (req,res)=>{
     })
 });
 
+app.get('/video', function(req, res) {
+    const path = 'assets/sample.mp4'
+    const stat = fileSystem.statSync(path)
+    const fileSize = stat.size
+    const range = req.headers.range
+    if (range) {
+      const parts = range.replace(/bytes=/, "").split("-")
+      const start = parseInt(parts[0], 10)
+      const end = parts[1] 
+        ? parseInt(parts[1], 10)
+        : fileSize-1
+      const chunksize = (end-start)+1
+      const file = fileSystem.createReadStream(path, {start, end})
+      const head = {
+        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': chunksize,
+        'Content-Type': 'video/mp4',
+      }
+      res.writeHead(206, head);
+      file.pipe(res);
+    } else {
+      const head = {
+        'Content-Length': fileSize,
+        'Content-Type': 'video/mp4',
+      }
+      res.writeHead(200, head)
+      fileSystem.createReadStream(path).pipe(res)
+    }
+  });
+
 module.exports = pdf
