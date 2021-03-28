c3a13336153d01622428e37a3a8d70317503c926
Afif Shaikh <shaikhafif48@gmail.com>
2021-03-28 15:19:06 +0530

    report page design updated

--- Project/immutables/src/app/report/report.component.ts
+++ Project/immutables/src/app/report/report.component.ts
@@ -9,9 +9,48 @@ import {MatAccordion} from '@angular/material/expansion';
 })
 export class ReportComponent implements OnInit {
   @ViewChild(MatAccordion) accordion: MatAccordion;
+
+  iskocOpen = false
+  isrocOpen = false
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  kocToggle(){
+    if(this.iskocOpen)
+      {
+        document.getElementById("kocTheoryRepresentation").style.display = "";
+        document.getElementById("kocGraphRepresentation").style.display = "none";
+        document.getElementById("kocGraphIcon").style.color = "#7A7E81";
+        this.iskocOpen = false
+      }
+      else
+      {
+        document.getElementById("kocTheoryRepresentation").style.display = "none"
+        document.getElementById("kocGraphRepresentation").style.display = "";
+        document.getElementById("kocGraphIcon").style.color = "#0165FF";
+        this.iskocOpen = true
+      }
+    }
+
+  rocToggle(){
+    if(this.isrocOpen)
+      {
+        document.getElementById("rocTheoryRepresentation").style.display = "";
+        document.getElementById("rocGraphRepresentation").style.display = "none";
+        document.getElementById("rocGraphIcon").style.color = "#7A7E81";
+        this.isrocOpen = false
+      }
+      else
+      {
+        document.getElementById("rocTheoryRepresentation").style.display = "none"
+        document.getElementById("rocGraphRepresentation").style.display = "";
+        document.getElementById("rocGraphIcon").style.color = "#0165FF";
+        this.isrocOpen = true
+      }
   }
 
 }
