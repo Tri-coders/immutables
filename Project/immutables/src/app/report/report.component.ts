06c0bafe479ec8a62e535aac5e34e947dbf45e36
Afif Shaikh <shaikhafif48@gmail.com>
2021-03-30 12:30:10 +0530

    report page all grpahs embeded

--- Project/immutables/src/app/report/report.component.ts
+++ Project/immutables/src/app/report/report.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewEncapsulation, ViewChild} from '@angular/core';
 import {MatAccordion} from '@angular/material/expansion';
-import { delay } from 'rxjs/operators';
+import { ChartDataSets, ChartOptions } from 'chart.js';
 
 @Component({
   selector: 'app-report',
@@ -51,16 +51,47 @@ export class ReportComponent implements OnInit {
         document.getElementById("rocTheoryRepresentation").style.display = "none"
         document.getElementById("rocGraphRepresentation").style.display = "";
         document.getElementById("rocGraphIcon").style.color = "#0165FF";
+        this.rocGraphStartAnimate();
         this.isrocOpen = true
       }
   }
 
   //ng bar chart charts
-    public barChartOptions = {        
+  //knwoledge of cognition graph
+    public kocbarChartOptions = {        
       scaleShowVerticalLines: false,
       responsive: true
     };
   
+    public kocbarChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+    public kocbarChartType = 'bar';
+    public kocbarChartLegend = true;
+  
+    public kocbarChartData = [
+      {data: [0, 0, 0, 0, 0, 0, 0], label: 'Series A'},
+      {data: [0, 0, 0, 0, 0, 0, 0], label: 'Series B'}
+    ];
+    
+    //regulation of cognition groph
+    public rocbarChartOptions = {        
+      scaleShowVerticalLines: false,
+      responsive: true
+    };
+  
+    public rocbarChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+    public rocbarChartType = 'bar';
+    public rocbarChartLegend = true;
+  
+    public rocbarChartData = [
+      {data: [0, 0, 0, 0, 0, 0, 0], label: 'Series A'},
+      {data: [0, 0, 0, 0, 0, 0, 0], label: 'Series B'}
+    ];
+
+    //planning component graph
+    public barChartOptions = {        
+      scaleShowVerticalLines: false,
+      responsive: true
+    };
     public barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
     public barChartType = 'bar';
     public barChartLegend = true;
@@ -72,15 +103,31 @@ export class ReportComponent implements OnInit {
 
 
     kocGraphStartAnimate(){
-      this.barChartOptions = {
+      this.kocbarChartOptions = {
         scaleShowVerticalLines: false,
         responsive: true
       };
     
-      this.barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-      this.barChartType = 'bar';
-      this.barChartLegend = true;
-      this.barChartData = [
+      this.kocbarChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+      this.kocbarChartType = 'bar';
+      this.kocbarChartLegend = true;
+      this.kocbarChartData = [
+        {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+        {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
+        {data: [48, 41, 20, 50, 90, 37, 30], label: 'Series C'}
+      ];
+    }
+
+    rocGraphStartAnimate(){
+      this.rocbarChartOptions = {
+        scaleShowVerticalLines: false,
+        responsive: true
+      };
+    
+      this.rocbarChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+      this.rocbarChartType = 'bar';
+      this.rocbarChartLegend = true;
+      this.rocbarChartData = [
         {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
         {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
         {data: [48, 41, 20, 50, 90, 37, 30], label: 'Series C'}
@@ -93,6 +140,31 @@ export class ReportComponent implements OnInit {
     public pieChartData = [0, 0, 0, 0];
     public pieChartType = 'pie';
 
+    //ng line chart
+    public lineChartData = [
+      {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+      {data: [85, 48, 66, 52, 78, 90, 85], label: 'Series B'}
+    ];
+
+    public lineChartLabels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+    public lineChartOptions =  {
+      responsive: true,
+    };
+
+    public lineChartLegend = true;
+    public lineChartType = 'line';
+
+    public lineChartColors = [
+      {
+        borderColor: 'black',
+        backgroundColor: 'rgba(255,0,0,0.0)',
+      },
+      {
+        borderColor: 'red',
+        backgroundColor: 'rgba(255,0,0,0.0)',
+      },
+    ];
+
     tabClick(tab) {
         console.log(tab);
         if(tab.index == 1){
@@ -101,6 +173,20 @@ export class ReportComponent implements OnInit {
             this.pieChartData = [120, 150, 180, 90];
             this.pieChartType = 'pie';
           }, 500);
+        }
+        else if(tab.index == 3){
+          this.barChartOptions = {        
+            scaleShowVerticalLines: false,
+            responsive: true
+          };
+          this.barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+          this.barChartType = 'bar';
+          this.barChartLegend = true;
+        
+          this.barChartData = [
+            {data: [32, 67, 23, 98, 45, 76, 87], label: 'Series A'},
+            {data: [56, 23, 76, 87, 98, 34, 78], label: 'Series B'}
+          ];
         }
     }
 
