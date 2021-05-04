a20cf3f52e19ac9c2cecd145d370954b20a0f39b
Prabodh <prabodh.shewalkar@gmail.com>
2021-05-04 13:49:52 +0530

    quiz changes done

--- Project/immutables/src/server/database/db.js
+++ Project/immutables/src/server/database/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 const db = {}
-const sequelize = new Sequelize("metacognition","root","",{
+const sequelize = new Sequelize("metacognition","root","immutables@2021",{
     host: "localhost",
     dialect: "mysql",
     operatorsAliases: false,
